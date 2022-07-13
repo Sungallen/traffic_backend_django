01302be1e7fff279e73192c8a61cba5e50a2a800
Sungallen <a0975671396@gmail.com>
2022-07-13 12:54:43 +0800

    forth

--- traffic_backend/schemas.py
+++ traffic_backend/schemas.py
@@ -6,11 +6,13 @@ class flowsType(DjangoObjectType):
     class Meta:
         model = flows
         fields = ("id", "category", "coordinates", "flow", "time")
+
 class categoryType(DjangoObjectType):
     class Meta:
         model = category
         fields = ("id","category")
-class coordinates(DjangoObjectType):
+
+class coordinatesType(DjangoObjectType):
     class Meta:
         model = coordinates
         fields = ("id","coordinates")
@@ -54,20 +56,22 @@ class FlowMultiMutations(graphene.Mutation):
     Is_Successed = graphene.Boolean()
     status = graphene.Int()
     class Arguments:
-        category = graphene.List(graphene.String, required=True)
-        coordinates = graphene.List(graphene.String, required=True)
+        in_category = graphene.List(graphene.String, required=True)
+        in_coordinates = graphene.List(graphene.String, required=True)
         flow = graphene.List(graphene.Int, required=True)
         time = graphene.DateTime(required=True)
         affectedrows = graphene.Int(required=True)
     @classmethod
-    def mutate(cls, root, info, category, coordinates, flow, time, affectedrows):
+    def mutate(cls, root, info, in_category, in_coordinates, flow, time, affectedrows):
         
 
         for i in range(affectedrows):
             try:
-                flows.objects.create(category=category[i], coordinates=coordinates[i], flow=flow[i], time=time)
+                temp_cat = category.objects.get(category=in_category[i])
+                temp_cor = coordinates.objects.get(coordinates=in_coordinates[i])
+                flows.objects.create(category=temp_cat, coordinates=temp_cor, flow=flow[i], time=time)
             except:
-                return FlowMultiMutations(Is_Successed=False, status=404)
+                return FlowMultiMutations(Is_Successed=False, status=400)
         return FlowMultiMutations(Is_Successed=True, status=200)
 
 class Mutation(graphene.ObjectType):
