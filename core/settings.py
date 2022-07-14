3ab706188e6927ef42ee0817a8d4fca0f21c710c
Sungallen <a0975671396@gmail.com>
2022-07-14 12:34:37 +0800

    export-import-flowsdata

--- core/settings.py
+++ core/settings.py
@@ -42,13 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'traffic_backend',
     'graphene_django',
+    'import_export'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- traffic_backend/admin.py
+++ traffic_backend/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import flows, category, coordinates
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
-admin.site.register(flows)
+class FlowsAdmin(ImportExportModelAdmin):
+    pass
+admin.site.register(flows, FlowsAdmin)
 admin.site.register(category)
 admin.site.register(coordinates)
