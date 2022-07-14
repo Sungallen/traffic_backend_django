613f7d8bb7f95e0eef00092eceacdb1ef5432f53
Sungallen <a0975671396@gmail.com>
2022-07-14 22:24:39 +0800

    admin_modification

--- traffic_backend/admin.py
+++ traffic_backend/admin.py
@@ -4,6 +4,14 @@ from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 class FlowsAdmin(ImportExportModelAdmin):
     pass
-admin.site.register(flows, FlowsAdmin)
-admin.site.register(category)
-admin.site.register(coordinates)
+#admin.site.register(flows, FlowsAdmin)
+admin.site.register(category, FlowsAdmin)
+
+@admin.register(coordinates)
+class coordinatesAdmin(FlowsAdmin):
+    list_display = ("descriptions", "coordinates")
+
+@admin.register(flows)
+class flowsAdmin(ImportExportModelAdmin):
+    list_display = ("category", "flow", "coordinates", "time")
+    search_fields = ["category__category", "time"]
